8f1939d9d440d19acfb3dc981c55cb385133bf30
Mumbaikar007 <brainteaser97@gmail.com>
2017-10-18 10:34:25 +0530

    Adarsh & Pranav

---
+++ stackoverflow.py
@@ -0,0 +1,20 @@
+import smtplib
+
+FROMADDR = "cmpnb.sfit2015@gmail.com"
+LOGIN    = FROMADDR
+PASSWORD = "Cmpnb_123"
+TOADDRS  = ["tanmay.sankhe97@gmail.com"]
+SUBJECT  = "Test"
+
+msg = ("From: %s\r\nTo: %s\r\nSubject: %s\r\n\r\n"
+       % (FROMADDR, ", ".join(TOADDRS), SUBJECT) )
+msg += "some text\r\n"
+
+server = smtplib.SMTP('smtp.gmail.com', 587)
+
+server.set_debuglevel(1)
+server.ehlo()
+server.starttls()
+server.login(LOGIN, PASSWORD)
+server.sendmail(FROMADDR, TOADDRS, msg)
+server.quit()
---
+++ stmpmime.py
@@ -0,0 +1,42 @@
+#imports
+
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.image import MIMEImage
+from email.mime.text import MIMEText
+import time
+
+
+# smtp gmail
+to = ""
+user = ""
+password = ""
+
+smtpserver = smtplib.SMTP('smtp.gmail.com', 587, timeout=30)
+print("Here")
+smtpserver.ehlo()
+smtpserver.starttls()
+smtpserver.ehlo()
+smtpserver.login ( user, password)
+
+print("Here")
+
+msg = MIMEMultipart()
+
+msg['To']=to
+msg['From']=user
+msg['Subject']='IOT'
+
+msg.attach(MIMEText("Hello"))
+
+print("Here")
+#mail
+fp=open('dog.png','rb')
+msg.attach(MIMEImage(fp.read()))
+print("Here")
+smtpserver.sendmail ( user, to , msg.as_string())
+print("Here")
+#smtpserver.close()
+
+print("mailed !!")
+
