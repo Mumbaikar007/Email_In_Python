8e2817fbafc94301d62d150ea4a1cb61ca37feee
Mumbaikar007 <brainteaser97@gmail.com>
2017-12-10 13:11:24 +0530

    Almost :D

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-from stmpmime import MailOject
+
+
 import sys
+import re
+import os
+from stmpmime import MailOject
 from PyQt5.QtWidgets import (QApplication, QComboBox, QDialog,
-        QDialogButtonBox, QFormLayout, QGridLayout, QGroupBox, QHBoxLayout,
-        QLabel, QLineEdit, QMenu, QMenuBar, QPushButton, QSpinBox, QTextEdit,
-        QVBoxLayout, QWidget)
+                             QDialogButtonBox, QFormLayout, QGridLayout, QGroupBox, QHBoxLayout,
+                             QLabel, QLineEdit, QMenu, QMenuBar, QPushButton, QSpinBox, QTextEdit,
+                             QVBoxLayout, QWidget, QFileDialog)
 
 
 
@@ -12,15 +16,25 @@ class EmailClient(QWidget):
     def __init__(self):
         super(EmailClient, self).__init__()
 
-        self.text_message = QTextEdit(self)
-
-        self.clr_btn = QPushButton('Clear')
-        self.send_mail_btn = QPushButton('Send')
-
         self.sender_email_le = QLineEdit()
         self.sender_password_le = QLineEdit()
         self.receiver_email_le = QLineEdit()
-        self.subject = QLineEdit()
+        self.subject_le = QLineEdit()
+
+        self.text_message = QTextEdit(self)
+
+        self.attachments_label = QLabel("No Attachments yet !!")
+        self.attachments_list = []
+
+        self.sender_email_le.setText("10abcdefghij01@gmail.com")
+        self.sender_password_le.setText("I can't tell you !!")
+        self.receiver_email_le.setText("spidermanavanger@gmail.com")
+        self.subject_le.setText("Stackoverflow3")
+        self.text_message.setPlainText("OKk !!")
+
+        self.clear_button = QPushButton('Clear')
+        self.insert_button = QPushButton('Insert')
+        self.send_mail_button = QPushButton('Send')
 
         self.init_ui()
 
@@ -44,25 +58,33 @@ class EmailClient(QWidget):
         formlayout.addRow(QLabel("Email: "), self.sender_email_le )
         formlayout.addRow(QLabel("Password: "), self.sender_password_le)
         formlayout.addRow(QLabel("To: "), self.receiver_email_le)
-        formlayout.addRow(QLabel("Subject: "), self.subject)
+        formlayout.addRow(QLabel("Subject: "), self.subject_le)
 
-        # Clear, Attach Image, Send
 
+        # horizontal 3: Attachments
+
+        h_layout3.addWidget(self.attachments_label)
+        h_layout3.addStretch()
+
+
+        # horizontal 4: Add Clear, Attach Image, Send
         h_layout4.addStretch(1)
-        h_layout4.addWidget(self.clr_btn)
-        h_layout4.addWidget(self.send_mail_btn)
+        h_layout4.addWidget(self.clear_button)
+        h_layout4.addWidget(self.insert_button)
+        h_layout4.addWidget(self.send_mail_button)
 
 
         # Design vertical layout
         v_layout.addLayout(formlayout)
         v_layout.addWidget(self.text_message)
+        v_layout.addLayout(h_layout3)
         v_layout.addLayout(h_layout4)
 
 
         # button clicks
-        self.send_mail_btn.clicked.connect(self.send_mail)
-        self.clr_btn.clicked.connect(self.clear_text)
-
+        self.send_mail_button.clicked.connect(self.send_mail)
+        self.clear_button.clicked.connect(self.clear_text)
+        self.insert_button.clicked.connect(self.insert_attachment)
 
 
         self.setLayout(v_layout)
@@ -72,17 +94,35 @@ class EmailClient(QWidget):
 
 
     def send_mail(self):
-        to, user, password, subject, message  = self.receiver_email_le.text(),\
+
+        mail_object = MailOject(self.receiver_email_le.text(),\
                                                 self.sender_email_le.text(),\
                                                 self.sender_password_le.text(), \
-                                                self.subject.text(),self.text_message.toPlainText()
-
-        mail_object = MailOject(to, user, password, subject, message)
+                                                self.subject_le.text(),\
+                                                self.text_message.toPlainText(),\
+                                                self.attachments_list
+                                )
         mail_object.Sending_Email()
 
 
+    def insert_attachment(self):
+
+        filename = QFileDialog.getOpenFileName(self, 'Open File', os.getenv('HOME'))
+        self.attachments_list.append(filename[0])
+
+
+        new_attachment_label = "Attachments: "
+        for f in self.attachments_list:
+            new_attachment_label += (os.path.basename(f) + ", ")
+
+        self.attachments_label.setText(new_attachment_label)
+
+
+
     def clear_text(self):
-        self.text.clear()
+        self.text_message.clear()
+
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
--- stmpmime.py
+++ stmpmime.py
@@ -1,49 +1,64 @@
-#imports
 
 import smtplib
+from os.path import basename
+from email.utils import formatdate
 from email.mime.multipart import MIMEMultipart
+from email.mime.application import MIMEApplication
 from email.mime.image import MIMEImage
 from email.mime.text import MIMEText
 import time
 
 class MailOject:
 
-    def __init__(self, to, user, password, subject, message):
+    def __init__(self, to, user, password, subject, message, attachment_list):
         self.to = to
         self.user = user
         self.password = password
         self.subject = subject
         self.message = message
+        self.attachment_list =attachment_list
 
     def Sending_Email(self):
-        # smtp gmail
+
 
 
         smtpserver = smtplib.SMTP('smtp.gmail.com', 587, timeout=30)
-        #print("Here")
+
         try:
             smtpserver.ehlo()
             smtpserver.starttls()
             smtpserver.ehlo()
             smtpserver.login ( self.user, self.password)
 
-            #print("Here")
-
             msg = MIMEMultipart()
 
             msg['To'] = self.to
             msg['From'] = self.user
+            msg['Date'] = formatdate(localtime=True)
             msg['Subject'] = self.subject
 
             msg.attach(MIMEText(self.message))
 
-            #print("Here")
-            #mail
-            fp=open('dog.png','rb')
-            msg.attach(MIMEImage(fp.read()))
-            #print("Here")
+
+            # mail attachments
+
+            for file in self.attachment_list:
+                print(file, basename(file))
+                rfile = open( file, 'rb')
+                part = MIMEApplication(
+                    rfile.read(),\
+                    Name=basename(file)
+                    )
+                part['Content-Disposition'] = 'attachment; filename="%s"' % basename(file)
+                msg.attach(part)
+
+            #fp = open('messi5.jpeg', 'rb')
+            #msg.attach(MIMEImage(fp.read()))
+
+
+
             smtpserver.sendmail ( self.user, self.to , msg.as_string())
-            #print("Here")
+
             smtpserver.close()
 
             print("mailed !!")
